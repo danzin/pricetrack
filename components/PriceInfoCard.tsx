82789fb55884ae0c765085e1e89d5ea598d888e4
danzin <excl.d.k@gmail.com>
2023-12-27 11:20:28 +0200

    chore: format price in PriceInfoCard component This commit refines the display of the price in the PriceInfoCard component by rounding it to two decimal places to ensure consistent formatting.

--- components/PriceInfoCard.tsx
+++ components/PriceInfoCard.tsx
@@ -8,12 +8,15 @@ interface PIProps {
 }
 
 const PriceInfoCard = ({title, iconSrc, value}: PIProps) => {
+  //Round the value to 3 decimal places
+  const formattedValue = parseFloat(value).toFixed(3); 
+  
   return (
     <div className={`price-info_card`}>
       <p className='text-base text-black-100'>{title}</p>
       <div className='flex gap-1'>
         <Image src={iconSrc} alt={title} width={24} height={24}/>
-        <p className='text-xl font-bold text-secondary'>{value}</p>
+        <p className='text-xl font-bold text-secondary'>{formattedValue}лв</p>
       </div>
     </div>
   )
